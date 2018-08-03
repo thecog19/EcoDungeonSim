9f12b21050ed00a6e3e932cb230f4a19489145ae
Felipe <suerofrancisco@yahoo.com>
2018-08-03 17:31:06 -0400

    restarting

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,17 @@
 extern crate rand;
+extern crate gtk;
 
-struct Dungeon {
-  map: Vec<Vec<i32>>;
-  seed: Vec<_>;
-}
+use gtk::prelude::*;
+use gtk::{Button, Window, WindowType};
 
-// pub struct Node<T> {
-//     parent: Option<NodeId>,
-//     previous_sibling: Option<NodeId>,
-//     next_sibling: Option<NodeId>,
-//     first_child: Option<NodeId>,
-//     last_child: Option<NodeId>,
 
-//     /// The actual data which will be stored within the tree
-//     pub data: T,
-// }
-
-// pub struct NodeId {
-//     index: usize,
-// }
-
-use rand::Rng;
 fn main() {
-  let seed: i32 = generate_seed();
-  build_dungeon(seed);
-}
-
-
-fn generate_seed() -> i32{
-    println!("Generating a random seed for dungeon generation");
-    let seed: i32 = rand::thread_rng().gen_range(10000000,99999999);
-    println!("Seed is: {}", seed);
-    return seed
-}
-
-
-
-fn build_dungeon(seed: i32){
-    let digits: Vec<_> = seed.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
-
-    let dungeon = Dungeon {
-      map: 
-      seed: digits
-    }
-    //the first character of the string is the default number of rooms
-    // now we generate 
-   // print_dungeon(dungeon);
-}
-
-fn assemble_layout(rooms: i32, seed: i32){
+  let seed: i64 = generate_seed();
+  println!("{:?}", seed);
   
 }
-fn print_dungeon(dungeon:  Vec<Vec<i32>> ){
-    for x in dungeon.iter(){
-        println!("{:?}", x);
-    }
+
+use rand::Rng;
+fn generate_seed() -> i64 {
+  return rand::thread_rng().gen_range(1_000_000_000, 10_000_000_000);
 }
